cd4a95af40170f1fd1eb103a6a0d15f7ae892317
squelch0 <45242451+squelch0@users.noreply.github.com>
2020-04-12 20:03:56 -0500

    Add files via upload

---
+++ stuf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+jsonfile=$1
+userstat=$2
+length=$(jq '.results|length' $jsonfile) 
+
+setUrl(){
+	url=$(jq .results[$cntr].url $jsonfile)
+}
+
+getUrl(){
+	echo "$url"|sed 's/"//g'
+}
+
+printStat(){
+cntr=0
+while [ $cntr -ne $length ]
+do
+	status=$(jq .results[$cntr].status $jsonfile)
+	setUrl
+	if [ $status -eq $userstat ]
+	then
+	        getUrl	
+		cntr=$((cntr+1))
+	else
+		cntr=$((cntr+1))
+	fi
+done
+}
+
+printAll(){
+cntr=0
+while [ $cntr -ne $length ]
+do
+	setUrl
+	getUrl
+	cntr=$((cntr+1))
+done
+}
+
+if [ ! $userstat ]
+then
+printAll
+fi
+
+if [ $userstat ]
+then
+printStat
+fi
